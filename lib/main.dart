29e67aa811efffdb6c1af9a50ff073cd790993a7
Suraj Naveen <suraj0nb@gmail.com>
2024-01-28 00:10:45 +0530

    Flutter UI
    First Page finished ui

---
+++ lib/main.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+void main() => runApp(
+      DevicePreview(
+        //enabled: !kReleaseMode,
+        builder: (context) => MyApp(), // Wrap your app
+      ),
+    );
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween, // Aligns children to the top and bottom
+            children: [
+              Column(
+                children: [
+                  SvgPicture.asset(
+                    "asset/Banknote-pana.svg",
+                    width: 250,
+                    height: 250,
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      "Let's Get Started",
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24, fontFamily: 'Nunito'),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.fromLTRB(25, 10, 25, 20),
+                    child: Text(
+                      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout",
+                      style: TextStyle(
+                        fontFamily: 'Nunito',
+                        fontSize: 16,
+                        color: Color.fromARGB(90, 0, 0, 0),
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  SizedBox(
+                    height: 60,
+                    width: 370,
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromRGBO(160, 32, 240, 1),
+                        ),
+                        child: const Text(
+                          "Create Account",
+                          style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold, fontFamily: 'Nunito', letterSpacing: 5, color: Color.fromARGB(255, 255, 255, 255)),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 60,
+                    width: 370,
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+                        ),
+                        child: const Text(
+                          "Login",
+                          style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold, fontFamily: 'Nunito', letterSpacing: 5, color: Color.fromARGB(255, 0, 0, 0)),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
